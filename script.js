e86a4341a44ba6678a84269777f1d69b81de365f
neo-30003 <dr.arrum@gmail.com>
2024-05-29 21:46:55 +0300

    Выполнил второй урок

---
+++ script.js
@@ -0,0 +1,15 @@
+const title = "Project";
+const screens = "Simple, Complex, Interactive";
+const screenPrice = 1000;
+const rollback = 20;
+const fullPrice = 20000;
+const adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(screenPrice);
+console.log(fullPrice);
+console.log(screens.toLowerCase().split(", "));
+console.log(20000 * (20 / 100));
