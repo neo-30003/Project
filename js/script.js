a99ca612bccd436621f5c1676d9154f77460728b
neo-30003 <dr.arrum@gmail.com>
2024-06-21 23:26:52 +0300

    Исправил вывод totalinput

--- js/script.js
+++ js/script.js
@@ -6,15 +6,15 @@ const otherItems1 = document.querySelectorAll(".other-items.percent");
 const otherItems2 = document.querySelectorAll(".other-items.number");
 const rollbackInput = document.querySelector(".rollback input");
 const rollbackSpan = document.querySelector(".rollback span");
-const totalInput = document.getElementsByClassName("total-input");
-
+const totalInput = function () {
+  let list = document.getElementsByClassName("total-input");
+  for (let i = 0; i < list.length; i++) {
+    console.log(list[i]);
+  }
+};
 let screen = document.querySelectorAll(".screen");
 const calculate = handlerBtn[0];
 const reset = handlerBtn[1];
-
-Array.from(totalInput).forEach((el) => {
-  console.log(el);
-});
 
 console.log(title[0]);
 console.log(calculate);
@@ -24,4 +24,5 @@ console.log(otherItems1);
 console.log(otherItems2);
 console.log(rollbackInput);
 console.log(rollbackSpan);
+console.log(totalInput());
 console.log(screen);
